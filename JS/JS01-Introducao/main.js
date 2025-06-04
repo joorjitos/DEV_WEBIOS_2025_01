2a3c2a115bca7ce3f6a6e5b5de5a7b61985418ab
joorjitos <jorge.assalin@gmail.com>
2025-06-03 21:38:29 -0300

    introdução JS

---
+++ JS/JS01-Introducao/main.js
@@ -0,0 +1,17 @@
+var num1 = 4
+var num2 = 4
+var txt = "resultado da conta:"
+var resultado = num1 + num2
+console.log(txt, resultado);
+
+var nascimento = 1979
+var ano = 2025
+var let = "Sua idade:"
+var total = ano - nascimento
+console.log(let, total);
+
+
+
+
+
+
